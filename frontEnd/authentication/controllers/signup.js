efe25d13a123478b0108d604f27e9f5b9841bafb
Evan Morrison <evanrmorrison@gmail.com>
2016-10-17 22:17:48 -0600

    add initial form validation on signup modal

--- frontEnd/authentication/controllers/signup.js
+++ frontEnd/authentication/controllers/signup.js
@@ -3,7 +3,16 @@
 angular.module('myApp.Auth')
     .controller('SignupController', ["$scope", "$location", "$timeout", "$uibModal", "$uibModalInstance", "UserService", function ($scope, $location, $timeout, $uibModal, $uibModalInstance, UserService) {
         $scope.user = {};
+        $scope.invalidForm = false;
         $scope.signup = function () {
+            if($scope.signupForm.$invalid) {
+                    $scope.invalidForm = true;
+                $timeout(function () {
+                    $scope.invalidForm = false;
+                }, 3500);
+                return;
+            }
+            else {
             UserService.signup($scope.user)
                 .then(function (response) {
                     $scope.success = response.success;
@@ -15,5 +24,6 @@ angular.module('myApp.Auth')
                         });
                     }, 2000);
                 })
+            }
         }
     }])
