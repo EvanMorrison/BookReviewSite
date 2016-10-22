2680270de47f1d2a71b96babadf18ccecde6ebff
Evan Morrison <evanrmorrison@gmail.com>
2016-10-22 17:19:36 -0600

    restored googlebooks api response handling

--- frontEnd/services/httpService.js
+++ frontEnd/services/httpService.js
@@ -8,36 +8,49 @@ app.service("HttpService", ["$http", "$httpParamSerializer", function ($http, $h
     this.searchGoogle = function (searchTerms) {
         console.log("searchTerms ", searchTerms);
         return $http.get('/services/config.googleBooksAPI.ignore.js')
-        .then(function (googleBooksAPIKey) {
-            return $http.get('https://www.googleapis.com/books/v1/volumes?q=' + $httpParamSerializer(searchTerms), googleBooksAPIKey.data )
-                .then(function (response) {
-                    console.log(response.data)
-                    response.data.items.forEach(function (item, index) {
-                        self.searchResults.push({
-                            title: item.volumeInfo.title,
-                            subtitle: item.volumeInfo.subtitle,
-                            author: item.volumeInfo.authors[0],
-                            publishedDate: item.volumeInfo.publishedDate,
-                            publisher: item.volumeInfo.publisher,
-                            previewLink: item.volumeInfo.previewLink,
-                            pageCount: item.volumeInfo.pageCount,
-                            infoLink: item.volumeInfo.infoLink,
-                            ISBN: item.volumeInfo.industryIdentifiers[0].identifier,
-                            thumbnail: item.volumeInfo.imageLinks.thumbnail,
-                            description: item.volumeInfo.description,
-                            textSnippet: item.searchInfo.textSnippet,
-                            // listPrice: {
-                            //     amount: item.saleInfo.listPrice.amount || 0,
-                            //     currencyCode: item.saleInfo.listPrice.currencyCode ||''}
+            .then(function (googleBooksAPIKey) {
+                return $http.get('https://www.googleapis.com/books/v1/volumes?q=' + $httpParamSerializer(searchTerms), googleBooksAPIKey.data)
+                    .then(function (response) {
+                        console.log(response.data)
+                        response.data.items.forEach(function (item, index) {
+                            self.searchResults.push({
+                                title: item.volumeInfo.title,
+                                subtitle: item.volumeInfo.subtitle,
+                                publishedDate: item.volumeInfo.publishedDate,
+                                publisher: item.volumeInfo.publisher,
+                                previewLink: item.volumeInfo.previewLink,
+                                pageCount: item.volumeInfo.pageCount,
+                                infoLink: item.volumeInfo.infoLink,
+                                description: item.volumeInfo.description,
+                                genres: item.volumeInfo.categories
+                            });
+                            if (item.volumeInfo.industryIdentifiers) {
+                                self.searchResults[index].ISBN = item.volumeInfo.industryIdentifiers[0].identifier
+                            }
+                            if (item.volumeInfo.imageLinks) {
+                                self.searchResults[index].thumbnail = item.volumeInfo.imageLinks.thumbnail;
+                            }
+
+                            if (item.volumeInfo.searchInfo) {
+                                self.searchResults[index].textSnippet = item.volumeInfo.searchInfo.textSnippet || ''
+                            }
+                            if (item.volumeInfo.authors) {
+                                self.searchResults[index].author = item.volumeInfo.authors[0]
+                            }
+                            if (item.saleInfo.listPrice) {
+                                self.searchResults[index].listPrice = {
+                                    amount: item.saleInfo.listPrice.amount,
+                                    currencyCode: item.saleInfo.listPrice.currencyCode
+                                }
+
+                            }
                         });
-                        // genres: item.volumeInfo.categories,
-                    });
-                    return self.searchResults;
-                }, function (error) {
-                    console.log(error);
-                    return (error);
-                })
-        })
+                        return self.searchResults;
+                    }, function (error) {
+                        console.log(error);
+                        return (error);
+                    })
+            })
     }
 
     this.saveBook = function (book) {
