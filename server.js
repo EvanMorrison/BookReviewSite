95240c3da9e9d405c53c6f347ffac375811abf73
Evan Morrison <evanrmorrison@gmail.com>
2017-02-13 23:01:28 -0700

    change folder name to match heroku branch

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // SERVE THE FRONTEND
-app.use(express.static(path.join(__dirname, '/frontend')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 
 // ROUTES REQUIRING AUTHENTICATION
